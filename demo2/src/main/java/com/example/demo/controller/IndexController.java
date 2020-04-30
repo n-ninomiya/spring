2b52484871ad03104cb95ba718f29b8255a0bcc5
n-ninomiya <naomedia330@gmail.com>
2020-04-30 16:43:38 +0900

    demo2追加

---
+++ demo2/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+
+	public String index(Model m) {
+		m.addAttribute("message", "Hello SpringBoot!");
+	  return "index";
+	}
+
+}
