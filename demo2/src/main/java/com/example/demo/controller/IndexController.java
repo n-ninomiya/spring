d9222703f901cedba5aa4b277e28adcd2433c3f3
n-ninomiya <naomedia330@gmail.com>
2020-05-14 18:16:13 +0900

    0514変更分

--- demo2/src/main/java/com/example/demo/controller/IndexController.java
+++ demo2/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
